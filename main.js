27a636fe81f0ad5607f2f96d90c784f2ae11458f
jhuang15 <jchuang1020@gmail.com>
2022-03-25 13:08:17 -0400

    testing commit

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
 /*----- constants -----*/
+const players = {
+	'1': {
+		name: '',
+		score: 0
+	},
+	'-1': {
+		name: '',
+		score: 0
+	}
+};
+const score = {
+
+};
+
+const turn = {
+	
+};
+
+
+
+
 /*----- app's state (variables) -----*/
+
+
+
+
 /*----- cached element references -----*/
+
+
+
+
 /*----- event listeners -----*/
+initialize();
+
+
+
 /*----- functions -----*/
+function initialize(){
+    render();
+}
+
+function render(){
+   /* renderHands();
+	renderControls();
+	if (winner) {
+		renderWinnerMessage();
+	} else {
+		renderTurnMessage();
+	}
+    */
+}
